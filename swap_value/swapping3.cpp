99e0a90c45f5d5aa5f6afc5bd8745a0d6384ca33
tanisha sadavarti <sadavartitanisha@gmail.com>
2025-02-01 10:28:37 +0530

    update-2

--- swap_value/swapping3.cpp
+++ swap_value/swapping3.cpp
@@ -6,9 +6,8 @@ main(){
 	int num1 = 10;
 	int num2 = 20;
 	
-	num2 = num2 ^ num1;
-	num1 = num1 ^ num2;
-	num2 = num1 ^ num2;
+	num1 = num2 * num1 /num1;
+	num2 = num2/2;
 	
 	cout << "num1 = " << num1 << endl << "num2 = " << num2;
 	
